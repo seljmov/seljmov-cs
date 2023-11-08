71bdb64b43a6e525cbbb7dc3e2341050506dddb1
Zagidin Selimov <seljmov@list.ru>
2023-11-08 15:28:53 +0300

    fix projects

--- Seljmov.CS/Structures/OwnArray.cs
+++ Seljmov.CS/Structures/OwnArray.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Seljmov.CS.Structures;
 
-namespace Seljmov.CS.Structures;
 public class OwnArray<T>
 {
     private T[]? _array;
